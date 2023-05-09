35b99f93b27e01d42cde5164dd437c0d81c6010c
CamargoFilho <camargofilho@outlook.com>
2023-05-08 22:22:58 -0300

    08-05-2023_22-22

--- 2023-1/Students/Cesar/List4/1-DivideConquer.py
+++ 2023-1/Students/Cesar/List4/1-DivideConquer.py
@@ -1,21 +1,19 @@
 # Divide-and-Conquer of an Array of n elements
 
-A = [8, 3, 2, 9, 7, 1, 5, 4];
+A = [8, 3, 2, 9, 7, 1, 5, 4, 22];
 A1 = [];
 A2 = [];
-i = 1;
+i = 0;
 tam = len(A);
-print("tam",tam)
+print("tam = ",tam)
 if tam % 2 != 0:
     div = int(tam/2)+1;
 else:
     div = int(tam/2);
 print("div = ",div)
-j = int(div);
-print("j = ",j)
 k = 1;
-BigNum1 = 1;
-BigNum2 = 2;
+BigNum1 = 0;
+BigNum2 = 0;
 
 def DivideConquer():
     for i in range(tam):
@@ -25,8 +23,21 @@ def DivideConquer():
             A2.append(A[i])
     print("A1 = ", A1)
     print("A2 = ", A2)
-
-
+    
+    j = int(div);
+    
+    for i in range(tam):
+        j = j-1
+        if j > 0:
+            print(j)
+            while A1[i] < A1[j]:  
+                if  A1[i] < A1[j]:
+                    print("A1[i] < A1[j] = ", A1[i],"<",A1[j])
+                    j = int(div);
+                    BigNum1 = int(A1[j])
+                else:
+                    BigNum1 = -1
+                
     
 
     if BigNum1 < BigNum2:
