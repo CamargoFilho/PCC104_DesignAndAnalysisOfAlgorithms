b8c7acb2ca517a1636912ce08dc7e7d7a533315f
CamargoFilho <camargofilho@outlook.com>
2023-05-10 18:59:54 -0300

    10-05-2023_18-59

--- 2023-1/Students/Cesar/List4/1-DivideConquer.py
+++ 2023-1/Students/Cesar/List4/1-DivideConquer.py
@@ -1,52 +1,47 @@
 # Divide-and-Conquer of an Array of n elements
 
-A = [8, 3, 2, 9, 7, 1, 5, 4, 22];
-A1 = [];
-A2 = [];
-i = 0;
-tam = len(A);
-print("tam = ",tam)
-if tam % 2 != 0:
-    div = int(tam/2)+1;
-else:
-    div = int(tam/2);
-print("div = ",div)
-k = 1;
-BigNum1 = 0;
-BigNum2 = 0;
+#Lista com n elementos
+A = [8, 3, 2, 9, 7, 1, 5, 4]; 
 
+#Cria o método Dividir e Conquistar
 def DivideConquer():
+
+    #Imprime a lista inicial
+    print("Lista A",A); 
+
+    #Cria sublista 1 vazia
+    A1 = []; 
+
+    #Cria sublista 2 vazia
+    A2 = []; 
+    
+    #Cria o contador do laço para dividir a lista em 2 sublistas
+    i = 0;
+    
+    #Mede o tamanho da lista inicial
+    tam = len(A);
+
+    #Calcula o resto da divisão por do total da lista por 2
+    mod = tam % 2
+    
+    #Compara se uma das sublistas irão ter qtd. de valores em par ou ímpar
+    if mod != 0: 
+        #Se o resto da divisão for diferente de zero, dividir uma das listas em qtd. de valores ímpar
+        div = int(tam/2)+mod;
+    else:
+        #Se o resto da divisão for igual a zero, dividir amba as listas em qtd. de valores par
+        div = int(tam/2);
+    
+    #Executa a divisão da lista inicial em 2 sublistas
     for i in range(tam):
         if i < div:
             A1.append(A[i])
         else:
             A2.append(A[i])
-    print("A1 = ", A1)
-    print("A2 = ", A2)
-    
-    j = int(div);
-    
-    for i in range(tam):
-        j = j-1
-        if j > 0:
-            print(j)
-            while A1[i] < A1[j]:  
-                if  A1[i] < A1[j]:
-                    print("A1[i] < A1[j] = ", A1[i],"<",A1[j])
-                    j = int(div);
-                    BigNum1 = int(A1[j])
-                else:
-                    BigNum1 = -1
-                
-    
+    print("Sublista A1 = ", A1)
+    print("Sublista A2 = ", A2)
 
-    if BigNum1 < BigNum2:
-        print("Lista =", A)
-        print("O maior número da lista acima é =", BigNum2)    
-    else:
-        print("Lista =", A)
-        print("O maior número da lista acima é =", BigNum1)    
-
+#Invoca o método Dividir e Conquistar
 DivideConquer();
 
 
